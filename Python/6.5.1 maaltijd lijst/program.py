e37bacddaacc12bbc1924b11006c171946f000ac
Jonas Claes <jonas@alterit.be>
2019-06-03 08:51:18 +0200

    Assignment 6.5.1: Meal list

---
+++ Python/6.5.1 maaltijd lijst/program.py
@@ -0,0 +1,3 @@
+maaltijden = ["Friet", "Friet met videe", "Friet met stoofvlees", "Friet met mayonaise", "Friet met ketchup"]
+
+print("De eerste maaltijd is \"%s\", en de tweede maaltijd is \"%s\"." % (maaltijden[0], maaltijden[maaltijden.__len__() - 1]))
