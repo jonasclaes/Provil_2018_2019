30c37ca942b5bbeeb05d731ee45151455d6e2a5a
Jonas Claes <jonas@alterit.be>
2019-06-03 08:51:43 +0200

    Assignment 6.5.4: All numbers

---
+++ Python/6.5.4 alle nummers/README.md
@@ -0,0 +1,3 @@
+# Assignment 6.5.4: Numbers
+
+Print out all the numbers from 1 to 1 million.
---
+++ Python/6.5.4 alle nummers/program.py
@@ -0,0 +1,6 @@
+getallen = list(range(1, 1000001))
+
+for getal in getallen:
+    print(getal)
+
+input("Press enter to stop...")
