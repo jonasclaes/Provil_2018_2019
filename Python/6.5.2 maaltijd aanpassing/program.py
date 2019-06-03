ce1f532d56f621ee4c3a43300f13a6824d50ab4b
Jonas Claes <jonas@alterit.be>
2019-06-03 08:51:28 +0200

    Assignment 6.5.2: Meal edit

---
+++ Python/6.5.2 maaltijd aanpassing/program.py
@@ -0,0 +1,7 @@
+maaltijden = ["Friet", "Friet met videe", "Friet met stoofvlees", "Friet met mayonaise", "Friet met ketchup"]
+
+del maaltijden[4]
+
+maaltijden.insert(1, "Steppegras")
+
+print(maaltijden)
